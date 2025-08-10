a54414fb95f4905481216f3d17a5b1bafec0a210
Teki-1 <nournabarawi20@gmail.com>
2025-08-10 18:06:29 +0300

    Added variables

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void  main (String[] args){
 
+        String[] choices= {"Rock","Paper","Scissors"};
+        Scanner scanner= new Scanner(System.in);
+        Random random = new Random();
+        String playerChoice;
+        String computerChoice;
+        String playAgain="YES";
+
+        System.out.println("Enter your move; (Rock, Paper, Scissors) ");
+        playerChoice=scanner.nextLine().substring(0,1).toUpperCase();
+
+
+
+
+        scanner.close();
     }
 }
